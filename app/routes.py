dfc9d448cd0e19b62740f3db37615416b929f7fb
Emilian Filip <emil.filip.dev@gmail.com>
2019-08-05 15:56:59 +0100

    Multi-page navigation via button
    Built a placeholder for the index page, added button which switches to the "page" page, added corresponding Flask route, changed run.bat due to firewall conflicts.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template("index.html")
+
+
+@app.route('/page')
+def page():
+    return render_template("page.html")
--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-python -m flask run --host="0.0.0.0" --port=80
+python -m flask run
