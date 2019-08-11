74f6aa8168dc6d6a8a7b3ee505c32698b821ef7f
Emilian Filip <emil.filip.dev@gmail.com>
2019-08-11 12:51:45 +0100

    Layout overhaul and Explanation tab content completed
    Changed the layout of pages so that content html files ONLY contain content rather than tables and navigation buttons. Finished the content of the Explanation tab, complete with image resources. Added bibliography and sources text files to keep track of usage.

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,21 @@
-from flask import send_from_directory, render_template
+import os
+from flask import send_from_directory, render_template, redirect
+from werkzeug.exceptions import NotFound
+
 from app import app
 
 
 @app.route("/")
-@app.route("/index")
 def index():
-    return render_template("main.html", content="/html/index.html")
+    return redirect("/index")
+
+
+@app.route("/<string:page>")
+def page(page):
+    return render_template("main.html",
+                           content="/html/" + page + ".html",
+                           backVisibility="hidden",
+                           nextVisibility="hidden")
 
 
 @app.route("/html/<path:file>")
@@ -23,9 +33,29 @@ def css(file):
     return send_from_directory("css", file)
 
 
-@app.route("/<string:tab>/<string:page>")
-def page(tab, page):
-    return render_template("main.html", content=("/html/" + tab + "/" + page + ".html"))
+@app.route("/resources/<path:file>")
+def rsrc(file):
+    return send_from_directory("resources", file)
+
+
+@app.route("/<string:tab>/<int:page>")
+def tabbed_page(tab, page):
+    back_page = page - 1
+    next_page = page + 1
+    back_visible = True
+    next_visible = True
+    if back_page == 0:
+        back_visible = False
+    try:
+        send_from_directory("html", tab + "/" + str(next_page) + ".html")
+    except NotFound:
+        next_visible = False
+    return render_template("main.html",
+                           content="/html/" + tab + "/" + str(page) + ".html",
+                           backLocation="/" + tab + "/" + str(back_page),
+                           nextLocation="/" + tab + "/" + str(next_page),
+                           backVisibility="visible" if back_visible else "hidden",
+                           nextVisibility="visible" if next_visible else "hidden")
 
 
 @app.route("/quiz")
